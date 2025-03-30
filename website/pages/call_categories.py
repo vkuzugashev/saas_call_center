9fe7430b5f06f11ccf4e5ae9c9364cc66de2e781
penart <vkuzugashev@mail.ru>
2025-03-30 18:13:02 +0700

    Отладка пользователей

--- website/pages/call_categories.py
+++ website/pages/call_categories.py
@@ -8,59 +8,89 @@ categories_bp = Blueprint('categories_bp', __name__, template_folder='../templat
 @categories_bp.route('/call_categories', methods=['GET', 'POST'])
 @login_required
 def call_categories():
-   """
-   Обработчик для отображения и добавления категорий вызовов.
+    """
+    Обработчик для отображения и добавления категорий вызовов.
 
-   Returns:
-       Response: Ответ сервера.
-   """
-   categories = CallCategory.query.all()
+    Returns:
+    Response: Ответ сервера.
+    """
+    categories = CallCategory.query.all()
 
-   if request.method == 'POST':
-       # Обработка формы для добавления новой категории
-       name = request.form.get('name')
+    if request.method == 'POST':
+        # Обработка формы для добавления новой категории
+        name = request.form.get('name')
 
-       if not name:
-           flash('Имя категории не может быть пустым.', 'danger')
-           return redirect(url_for('categories_bp.call_categories'))
+        # Проверка наличия имени категории
+        if not name:
+            flash('Имя категории не может быть пустым.', 'danger')
+            return redirect(url_for('categories_bp.call_categories'))
+        
+        # Проверка, что категория с таким именем не существует
+        existing_category = CallCategory.query.filter_by(name=name).first()
+        if existing_category:
+            flash(f'Категория [{name}] уже существует.', 'danger')
+            return redirect(url_for('categories_bp.call_categories'))
+       
+        # Создание новой категории вызова
+        new_category = CallCategory(name=name)
+        db.session.add(new_category)
+        db.session.commit()
+        flash(f'Категория [{name}] успешно добавлена!', 'success')
+        return redirect(url_for('categories_bp.call_categories'))
 
-       new_category = CallCategory(name=name)
-       db.session.add(new_category)
-       db.session.commit()
-       flash(f'Категория [{name}] успешно добавлена!', 'success')
-       return redirect(url_for('categories_bp.call_categories'))
-
-   return render_template('call_categories.html', categories=categories)
+    return render_template('call_categories.html', categories=categories)
 
 
 @categories_bp.route('/call_categories/edit/<int:id>', methods=['GET', 'POST'])
 @login_required
 def edit_call_category(id):
-   """
-   Обработчик для редактирования категории вызова.
+    """
+    Обработчик для редактирования категории вызова.
 
-   Args:
-       id (int): Идентификатор категории вызова, которую нужно редактировать.
+    Args:
+        id (int): Идентификатор категории вызова, которую нужно редактировать.
 
-   Returns:
-       Response: Ответ сервера.
-   """
-   category = CallCategory.query.get_or_404(id)
+    Returns:
+        Response: Ответ сервера.
+    """
+    category = CallCategory.query.get_or_404(id)
 
-   if request.method == 'POST':
-       # Обработка формы для редактирования категории
-       name = request.form.get('name')
+    if request.method == 'POST':
+        # Обработка формы для редактирования категории
+        newid = int(request.form.get('newid', 0))
+        name = request.form.get('name')
+        
+        # Проверка наличия идентификатора категории
+        if not newid:
+            flash('Идентификатор категории не может быть пустым.', 'danger')
+            return redirect(url_for('categories_bp.edit_call_category', id=id))
+        
+        if id != newid:
+            # Проверка наличия дубликтов категории с таким же id
+            existing_category = CallCategory.query.filter_by(id=newid).first()
+            if existing_category:
+                flash(f'Категория [{existing_category.name}] уже существует с таким id [{newid}].', 'danger')
+                return redirect(url_for('categories_bp.edit_call_category', id=id))
+            
+        # Проверка наличия имени категории
+        if not name:
+            flash('Имя категории не может быть пустым.', 'danger')
+            return redirect(url_for('categories_bp.edit_call_category', id=id))
 
-       if not name:
-           flash('Имя категории не может быть пустым.', 'danger')
-           return redirect(url_for('categories_bp.edit_call_category', id=id))
+        if id == newid:
+            # Проверка наличия дубликтов категории с таким же именем
+            existing_category = CallCategory.query.filter_by(name=name).first()
+            if existing_category:
+                flash(f'Категория [{name}] уже существует.', 'danger')
+                return redirect(url_for('categories_bp.edit_call_category', id=id))
+       
+        category.id = newid
+        category.name = name
+        db.session.commit()
+        flash(f'Категория [{category.name}] успешно отредактирована!', 'success')
+        return redirect(url_for('categories_bp.call_categories'))
 
-       category.name = name
-       db.session.commit()
-       flash(f'Категория [{category.name}] успешно отредактирована!', 'success')
-       return redirect(url_for('categories_bp.call_categories'))
-
-   return render_template('edit_call_category.html', category=category)
+    return render_template('edit_call_category.html', category=category)
 
 
 @categories_bp.route('/call_categories/delete/<int:id>', methods=['GET'])
