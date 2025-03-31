03750aae9764558dc167ee0a7c3599bb351483dc
penart <vkuzugashev@mail.ru>
2025-03-31 13:20:45 +0700

    Добавил сервисы в скрипт

--- scripts/service_control.py
+++ scripts/service_control.py
@@ -30,14 +30,14 @@ def create_image(image_name, dockerfile_path):
    Returns:
        None
    """
-   command = ['docker', 'build', '-t', image_name, '-f', dockerfile_path, '.']
+   command = ['docker', 'build', '-t', image_name, dockerfile_path]
    result = subprocess.run(command, capture_output=True, text=True)
    if result.returncode != 0:
        raise RuntimeError(f"Ошибка при создании образа: {result.stderr}")
    else:
        print(f"Образ {image_name} успешно создан.")
 
-def create_container(container_name, image_name, env_vars):
+def create_container(container_name, image_name, env_vars, ports):
    """
    Функция для создания Docker контейнера.
 
@@ -45,6 +45,7 @@ def create_container(container_name, image_name, env_vars):
        container_name (str): Имя контейнера.
        image_name (str): Имя образа.
        env_vars (dict): Словарь с переменными окружения.
+       ports (dict): Словарь с открытыми портами.
 
    Returns:
        None
@@ -53,12 +54,35 @@ def create_container(container_name, image_name, env_vars):
    for key, value in env_vars.items():
        env_args.extend(['-e', f'{key}={value}'])
 
-   command = ['docker', 'create', '--name', container_name] + env_args + [image_name]
+   port_args = []
+   for host_port, container_port in ports.items():
+       port_args.extend(['-p', f'{host_port}:{container_port}'])
+
+   command = ['docker', 'create', '--name', container_name] + env_args + port_args + [image_name]
    result = subprocess.run(command, capture_output=True, text=True)
    if result.returncode != 0:
        raise RuntimeError(f"Ошибка при создании контейнера: {result.stderr}")
    else:
        print(f"Контейнер {container_name} успешно создан.")
+
+def remove_container(container_name):
+   """
+   Функция для удаления Docker контейнера.
+
+   Args:
+      container_name (str): Имя контейнера.
+
+   Returns:
+      None
+      """
+   # Удаление контейнера
+   command = ['docker', 'rm', container_name]
+   result = subprocess.run(command, capture_output=True, text=True)
+   if result.returncode != 0:
+      raise RuntimeError(f"Ошибка при удалении контейнера: {result.stderr}")
+   else:
+      print(f"Контейнер {container_name} успешно удален.")
+
 
 def start_container(container_name):
    """
@@ -98,28 +122,70 @@ def build():
    """
    Функция для создания сервисов.
    """
-   # Генерация пароля для root пользователя БД
-   db_password = generate_password()
+   rabbitmq_path = os.path.join(SERVICE_PATH, 'rabbitmq')
+   redis_path = os.path.join(SERVICE_PATH, 'redis')
+   mariadb_path = os.path.join(SERVICE_PATH, 'mariadb')
+   app_ami_path = os.path.join(SERVICE_PATH, 'app_ami')
+   app_call_path = os.path.join(SERVICE_PATH, 'app_call')
+   app_db_path = os.path.join(SERVICE_PATH, 'app_db')
+   app_new_call_path = os.path.join(SERVICE_PATH, 'app_new_call')
+   app_rcv_path = os.path.join(SERVICE_PATH, 'app_rcv_trns')
+   app_snd_path = os.path.join(SERVICE_PATH, 'app_snd_trns')
 
-   rabbitmq_path = os.path.join(SERVICE_PATH, 'rabbitmq/dockerfile')
-   redis_path = os.path.join(SERVICE_PATH, 'redis/dockerfile')
-   mariadb_path = os.path.join(SERVICE_PATH, 'mariadb/dockerfile')
+   db_password = os.environ.get('MARIADB_ROOT_PASSWORD')
+   if not db_password:
+      # Генерация пароля для root пользователя БД
+      db_password = generate_password()
+      # Запись пароля в .env файл
+      with open('.env', 'w') as f:
+         f.write(f'MARIADB_ROOT_PASSWORD={db_password}\n')   
+
+   # Остановка и удаление контейнеров
+   stop_container('rabbitmq')
+   stop_container('redis')
+   stop_container('mariadb')
+   stop_container('app_ami')
+   stop_container('app_call')
+   stop_container('app_db')
+   stop_container('app_new_call')
+   stop_container('app_rcv')
+   stop_container('app_snd')
+
+   # Удаление контейнеров
+   remove_container('rabbitmq')
+   remove_container('redis')
+   remove_container('mariadb')
+   remove_container('app_ami')
+   remove_container('app_call')
+   remove_container('app_db')
+   remove_container('app_new_call')
+   remove_container('app_rcv')
+   remove_container('app_snd')
 
    # Создание образов
    create_image('rabbitmq', rabbitmq_path)
    create_image('redis', redis_path)
    create_image('mariadb', mariadb_path)
-
-   # Запись пароля в .env файл
-   with open('.env', 'w') as f:
-       f.write(f'MARIADB_ROOT_PASSWORD={db_password}\n')
+   create_image('app_ami', app_ami_path)
+   create_image('app_call', app_call_path)
+   create_image('app_db', app_db_path)
+   create_image('app_new_call', app_new_call_path)
+   create_image('app_rcv', app_rcv_path)
+   create_image('app_snd', app_snd_path)
 
    # Запуск контейнеров
-   create_container('rabbitmq', 'rabbitmq', {})
-   create_container('redis', 'redis', {})
+   create_container('rabbitmq', 'rabbitmq', {},{})
+   create_container('redis', 'redis', {}, {'6379': '6379'})
    create_container('mariadb', 'mariadb', {
        'MARIADB_ROOT_PASSWORD': db_password,
-       'MARIADB_DATABASE': 'call_center'})
+       'MARIADB_DATABASE': 'call_center'},
+       {'3306': '3306'})   
+   create_container('app_ami', 'app_ami', {},{})
+   create_container('app_call', 'app_call', {},{})
+   create_container('app_db', 'app_db', {},{})
+   create_container('app_new_call', 'app_new_call', {},{})
+   create_container('app_rcv', 'app_rcv', {},{})
+   create_container('app_snd', 'app_snd', {},{})
 
 def start():
    """
@@ -129,6 +195,12 @@ def start():
    start_container('rabbitmq')
    start_container('redis')
    start_container('mariadb')
+   start_container('app_ami')
+   start_container('app_call')
+   start_container('app_db')
+   start_container('app_new_call')
+   start_container('app_rcv')
+   start_container('app_snd')
 
 def stop():
    """
@@ -138,6 +210,12 @@ def stop():
    stop_container('rabbitmq')
    stop_container('redis')
    stop_container('mariadb')
+   stop_container('app_ami')
+   stop_container('app_call')
+   stop_container('app_db')
+   stop_container('app_new_call')
+   stop_container('app_rcv')
+   stop_container('app_snd')
 
 if __name__ == '__main__':
    if len(sys.argv) != 2:
